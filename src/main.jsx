ee5386d9ff373bc86a955aaed97b7096016a6500
Abubakr <thefox5000@gmail.com>
2023-11-11 09:01:07 +0300

    Fix chakra UI configuration with vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Chakra UI Setup
+// import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+// import * as ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
